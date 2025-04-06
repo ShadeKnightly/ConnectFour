813f0bbe5601b05413503e71a0bb314c6d4c7068
Andrei Laqui <dreilaqui@yahoo.com>
2025-04-06 15:09:52 -0600

    Add files via upload

--- connect4UI/connect4UI/Pvp.cs
+++ connect4UI/connect4UI/Pvp.cs
@@ -5,53 +5,55 @@ using System.Data;
 using System.Data.Common;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using connect4UI.Properties;
 
 
 namespace connect4UI
 {
-
     public class Player
     {
         public string Name;
         public Color TokenColor;
         public int Score;
 
-
         public Player(string name, Color tokenColor, int score=0)
         {
             Name = name;
             TokenColor = tokenColor;
             Score = score;
-
         }
 
     }
 
-    public class Player1 : Player
+    public class RedPlayer : Player
     {
-        public Player1() : base("Player 1", Color.Red) 
-        { 
-        }
-    }
-
-    public class Player2 : Player
-    {
-        public Player2() : base("Player 2", Color.Yellow) 
+        public RedPlayer() : base("Red Player", Color.Red)
         {
         }
     }
 
+    public class YellowPlayer : Player
+    {
+        public YellowPlayer() : base("Yellow Player", Color.Yellow)
+        {
+        }
+    }
+
+
+
+
     public partial class Connect4PvP : Form
     {
         private Button[,] board = new Button[6, 7]; // board array 6 rows, 7 columns
-        private Color _currentPlayerColor = Color.Red; // Start with red
+        //private Color _currentPlayerColor = Color.Red; // Start with red
         private bool IsGameOver = false;
+        private Player currentPlayer, player1, player2; 
 
         public Connect4PvP()
         {
@@ -61,83 +63,27 @@ namespace connect4UI
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             // Initialize the board with your buttons (row 0 is top, row 5 is bottom)
-            board[0, 0] = Col0Row0;
-            board[1, 0] = Col0Row1;
-            board[2, 0] = Col0Row2;
-            board[3, 0] = Col0Row3;
-            board[4, 0] = Col0Row4;
-            board[5, 0] = Col0Row5;
+            for (int row = 0; row < 6; row++)
+            {
+                for (int col = 0; col < 7; col++)
+                {
+                    board[row, col] = (Button)this.Controls.Find($"Col{col}Row{row}", true)[0];
+                }
+            }
 
-            board[0, 1] = Col1Row0;
-            board[1, 1] = Col1Row1;
-            board[2, 1] = Col1Row2;
-            board[3, 1] = Col1Row3;
-            board[4, 1] = Col1Row4;
-            board[5, 1] = Col1Row5;
+            foreach (var button in new[] { Col0Btn, Col1Btn, Col2Btn, Col3Btn, Col4Btn, Col5Btn, Col6Btn })
+            {
+                // Attach event handlers only once in this initialization step
+                button.Click -= ColumnButton_Click;// Unsubscribe just in case it's subscribed multiple times
+                button.Click += ColumnButton_Click;
 
-            board[0, 2] = Col2Row0;
-            board[1, 2] = Col2Row1;
-            board[2, 2] = Col2Row2;
-            board[3, 2] = Col2Row3;
-            board[4, 2] = Col2Row4;
-            board[5, 2] = Col2Row5;
+                // Tag buttons so we know which column each belongs to
+                button.Tag = Array.IndexOf(new[] { Col0Btn, Col1Btn, Col2Btn, Col3Btn, Col4Btn, Col5Btn, Col6Btn }, button);
+            }
 
-            board[0, 3] = Col3Row0;
-            board[1, 3] = Col3Row1;
-            board[2, 3] = Col3Row2;
-            board[3, 3] = Col3Row3;
-            board[4, 3] = Col3Row4;
-            board[5, 3] = Col3Row5;
-
-            board[0, 4] = Col4Row0;
-            board[1, 4] = Col4Row1;
-            board[2, 4] = Col4Row2;
-            board[3, 4] = Col4Row3;
-            board[4, 4] = Col4Row4;
-            board[5, 4] = Col4Row5;
-
-            board[0, 5] = Col5Row0;
-            board[1, 5] = Col5Row1;
-            board[2, 5] = Col5Row2;
-            board[3, 5] = Col5Row3;
-            board[4, 5] = Col5Row4;
-            board[5, 5] = Col5Row5;
-
-            board[0, 6] = Col6Row0;
-            board[1, 6] = Col6Row1;
-            board[2, 6] = Col6Row2;
-            board[3, 6] = Col6Row3;
-            board[4, 6] = Col6Row4;
-            board[5, 6] = Col6Row5;
-
-            // Tag buttons so we know which column each belongs to
-            Col0Btn.Tag = 0;
-            Col1Btn.Tag = 1;
-            Col2Btn.Tag = 2;
-            Col3Btn.Tag = 3;
-            Col4Btn.Tag = 4;
-            Col5Btn.Tag = 5;
-            Col6Btn.Tag = 6;
-
-            // Attach event handlers only once in this initialization step
-            Col0Btn.Click -= ColumnButton_Click; // Unsubscribe just in case it's subscribed multiple times
-            Col1Btn.Click -= ColumnButton_Click;
-            Col2Btn.Click -= ColumnButton_Click;
-            Col3Btn.Click -= ColumnButton_Click;
-            Col4Btn.Click -= ColumnButton_Click;
-            Col5Btn.Click -= ColumnButton_Click;
-            Col6Btn.Click -= ColumnButton_Click;
-
-            Col0Btn.Click += ColumnButton_Click;
-            Col1Btn.Click += ColumnButton_Click;
-            Col2Btn.Click += ColumnButton_Click;
-            Col3Btn.Click += ColumnButton_Click;
-            Col4Btn.Click += ColumnButton_Click;
-            Col5Btn.Click += ColumnButton_Click;
-            Col6Btn.Click += ColumnButton_Click;
-
-
+            if (currentPlayer == null) CoinToss();
         }
 
         private void GameBoard_Paint(object sender, PaintEventArgs e)
@@ -145,8 +91,23 @@ namespace connect4UI
 
         }
 
-
-
+        private void CoinToss()
+        {
+            int randomPlayer = RandomNumberGenerator();
+            if (randomPlayer == 0)
+            {
+                player1 = new RedPlayer();
+                MessageBox.Show("Red goes first!");
+                player2 = new YellowPlayer();
+            }
+            else
+            {
+                player1 = new YellowPlayer();
+                MessageBox.Show("Yellow goes first!");
+                player2 = new RedPlayer();
+            }
+            currentPlayer = player1;
+        }
 
         private void ColumnButton_Click(object sender, EventArgs e)
         {
@@ -173,7 +134,9 @@ namespace connect4UI
             // Check for a winner
             if (CheckWin())
             {
-                MessageBox.Show($"Player {_currentPlayerColor} wins!");
+                currentPlayer.Score++;
+
+                MessageBox.Show($"{currentPlayer.Name} wins! Current score: {currentPlayer.Score}.");
                 IsGameOver = true;
                 return;
             }
@@ -187,7 +150,7 @@ namespace connect4UI
 
 
             // If valid move, no winner yet, not even a draw, let other player have a turn
-            _currentPlayerColor = (_currentPlayerColor == Color.Red) ? Color.Yellow : Color.Red;  // Switch players
+            currentPlayer = (currentPlayer == player1) ? player2 : player1;
         }
 
         private bool MakeMove(int column)
@@ -198,7 +161,7 @@ namespace connect4UI
                 if (board[row, column].BackColor == Color.White)
                 {
                     Debug.WriteLine($"Filling Row: {row}, Column: {column}");
-                    board[row, column].BackColor = _currentPlayerColor;  // fill the spot
+                    board[row, column].BackColor = currentPlayer.TokenColor;  // fill the spot
                     return true;
                 }
             }
@@ -213,14 +176,15 @@ namespace connect4UI
 
         private bool CheckHorizontalWin()
         {
+            var currColor = currentPlayer.TokenColor;
             for (int row = 0; row < 6; row++)
             {
                 for (int col = 0; col < 4; col++) // Only check up to column 4
                 {
-                    if (board[row, col].BackColor == _currentPlayerColor &&
-                        board[row, col + 1].BackColor == _currentPlayerColor &&
-                        board[row, col + 2].BackColor == _currentPlayerColor &&
-                        board[row, col + 3].BackColor == _currentPlayerColor)
+                    if (board[row, col].BackColor == currColor &&
+                        board[row, col + 1].BackColor == currColor &&
+                        board[row, col + 2].BackColor == currColor &&
+                        board[row, col + 3].BackColor == currColor)
                     {
                         return true;
                     }
@@ -231,14 +195,15 @@ namespace connect4UI
 
         private bool CheckVerticalWin()
         {
+            var currColor = currentPlayer.TokenColor;
             for (int col = 0; col < 7; col++)
             {
                 for (int row = 0; row < 3; row++) // Only check up to row 3
                 {
-                    if (board[row, col].BackColor == _currentPlayerColor &&
-                        board[row + 1, col].BackColor == _currentPlayerColor &&
-                        board[row + 2, col].BackColor == _currentPlayerColor &&
-                        board[row + 3, col].BackColor == _currentPlayerColor)
+                    if (board[row, col].BackColor == currColor &&
+                        board[row + 1, col].BackColor == currColor &&
+                        board[row + 2, col].BackColor == currColor &&
+                        board[row + 3, col].BackColor == currColor)
                     {
                         return true;
                     }
@@ -249,26 +214,29 @@ namespace connect4UI
 
         private bool CheckDiagonalWin()
         {
+            var currColor = currentPlayer.TokenColor;
             // Check for diagonal wins (both directions)
-            for (int row = 0; row < 3; row++)
+            for (int row = 0; row <= 5; row++)
             {
-                for (int col = 0; col < 4; col++)
+                for (int col = 0; col <=6 ; col++)
                 {
                     // Check diagonal down-right
-                    if (board[row, col].BackColor == _currentPlayerColor &&
-                        board[row + 1, col + 1].BackColor == _currentPlayerColor &&
-                        board[row + 2, col + 2].BackColor == _currentPlayerColor &&
-                        board[row + 3, col + 3].BackColor == _currentPlayerColor)
+                    if (row <= 2 && col <= 3 &&
+                        board[row, col].BackColor == currColor &&
+                        board[row + 1, col + 1].BackColor == currColor &&
+                        board[row + 2, col + 2].BackColor == currColor &&
+                        board[row + 3, col + 3].BackColor == currColor)
+
                     {
                         return true;
                     }
 
                     // Check diagonal down-left
-                    if (col >= 3 &&
-                        board[row, col].BackColor == _currentPlayerColor &&
-                        board[row + 1, col - 1].BackColor == _currentPlayerColor &&
-                        board[row + 2, col - 2].BackColor == _currentPlayerColor &&
-                        board[row + 3, col - 3].BackColor == _currentPlayerColor)
+                    if (row <= 2 && col >= 3 &&
+                        board[row, col].BackColor == currColor &&
+                        board[row + 1, col - 1].BackColor == currColor &&
+                        board[row + 2, col - 2].BackColor == currColor &&
+                        board[row + 3, col - 3].BackColor == currColor) 
                     {
                         return true;
                     }
@@ -292,13 +260,16 @@ namespace connect4UI
 
         private int RandomNumberGenerator()
         {
-            // Random number generator to determine the first player
             Random rnd = new Random();
 
             var num = rnd.Next(2);
 
             return num;
         }
+
+
+
+        
 
 
         private void Col0Row0_Click(object sender, EventArgs e)
@@ -328,21 +299,6 @@ namespace connect4UI
         }
 
         private void Col0Row5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Col3Btn_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Col4Btn_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Col5Btn_Click_1(object sender, EventArgs e)
         {
 
         }
