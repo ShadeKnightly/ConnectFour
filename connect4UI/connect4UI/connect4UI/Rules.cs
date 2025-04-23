be5c40c54419f51a1ef5d2b42b0d5f43994406a4
ShadeKnightly <147964491+ShadeKnightly@users.noreply.github.com>
2025-04-22 21:38:05 -0600

    Add files via upload

---
+++ connect4UI/connect4UI/connect4UI/Rules.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace connect4UI
+{
+    public partial class Rules : Form
+    {
+        public Rules()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rules_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
